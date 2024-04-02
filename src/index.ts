7d6b1c15df9e5a6baaf705e4e5314ffc188fa028
Benoît Hubert <benoithubert@gmail.com>
2024-04-02 10:29:33 +0200

    Add morgan middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import { port } from './settings';
 
 const app = express();
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.get('/api/dummy-endpoint', (req, res) =>
